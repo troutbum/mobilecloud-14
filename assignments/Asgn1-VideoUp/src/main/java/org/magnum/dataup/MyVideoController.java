69a687c7f04602eb64172ffe0b36e845aab41d44
troutbum <genec2008@gmail.com>
2014-08-17 17:10:52 -0700

    MyVideoController.java
    started

---
+++ assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/MyVideoController.java
@@ -0,0 +1,51 @@
+// Derived from Example 3-VideoControllerAndRetrofit
+
+package org.magnum.dataup;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import org.magnum.dataup.model.Video;
+
+// import org.magnum.mobilecloud.video.client.VideoSvcApi;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyVideoController {
+
+	// An in-memory list that the servlet uses to store the
+	// videos that are sent to it by clients
+	private List<Video> videos = new CopyOnWriteArrayList<Video>();
+
+	// The VIDEO_SVC_PATH is set to "/video" in the VideoSvcApi
+	// interface. We use this constant to ensure that the 
+	// client and service paths for the VideoSvc are always
+	// in synch.
+	public static final String VIDEO_SVC_PATH = "/video";
+	
+	// Receives POST requests to /video and converts the HTTP
+	// request body, which should contain json, into a Video
+	// object before adding it to the list. The @RequestBody
+	// annotation on the Video parameter is what tells Spring
+	// to interpret the HTTP request body as JSON and convert
+	// it into a Video object to pass into the method. The
+	// @ResponseBody annotation tells Spring to convert the
+	// return value from the method back into JSON and put
+	// it into the body of the HTTP response to the client.
+	@RequestMapping(value=VIDEO_SVC_PATH, method=RequestMethod.POST)
+	public @ResponseBody boolean addVideo(@RequestBody Video v){
+		return videos.add(v);
+	}
+	
+	// Receives GET requests to /video and returns the current
+	// list of videos in memory. Spring automatically converts
+	// the list of videos to JSON because of the @ResponseBody
+	// annotation.
+	@RequestMapping(value=VIDEO_SVC_PATH, method=RequestMethod.GET)
+	public @ResponseBody List<Video> getVideoList(){
+		return videos;
+	}
+}
